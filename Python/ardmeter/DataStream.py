e302dafd780835ea3d273b10f3c7f5f1658e266e
Anton <antosnail@yahoo.ie>
2018-06-19 23:32:37 +0100

    Changed datastream to return numpys but store in lists, verified basic function

--- Python/ardmeter/DataStream.py
+++ Python/ardmeter/DataStream.py
@@ -1,93 +1,89 @@
 import numpy as np
 
-""" Provides data in a "stream" """
+""" Provides data in a 'stream' """
 class DataStream:
-
-	""" Holds a measurement value and time """
-	class Measurement:
-		def __init__(self, time_, value_):
-			self.t = time_
-			self.v = value_
-
 	def __init__(self):
+		# initiate serial
+		self.initializeCommunication()
+		
 		# outstanding values that have just come in
-		self.current = list()
-		self.voltage = list()
-		self.power = list()
+		self._current = list()
+		self._voltage = list()
+		self._power = list()
+		self._time = list()
 
 		# total history that has been read
-		self.current_history = list()
-		self.voltage_history = list()
-		self.power_history = list()
+		self._current_history = list()
+		self._voltage_history = list()
+		self._power_history = list()
+		self._time_history = list()
 
-	def measurementsFromList(values, times = None):
+	def _measurementsFromArray(self, values):
+		# check that array is 1-D
+		assert(len(values.shape) == 1, 'Measurements are not 1-D')
+
 		measurements = list()
-		
-		# check if values holds both time and value
-		if times is None:
-			if isinstance(values, np.ndarray):
-				assert(values.shape[1] == 2, "No times provided with measurements")
-
-				for x in range(len(values[:, 0])):
-					list.append(Measurement(values[x, 0], values[x, 1]))
-			else
-				assert(False, "No times provided with measurements")
-		# else they are separate
-		else:
-			# ensure both vectors are same length
-			assert(len(values) == len(times))
-
-			for x in range(len(values)):
-				list.append(Measurement(values[x], times[x]))
+		for x in range(len(values)):
+			measurements.append(float(values[x]))
 
 		return measurements
 
-	# pull in measurements from arduino
 	# TODO actually pull in the measurements
-	def updateMeasurements(self):
+	def _updateMeasurements(self):
+		""" get all recent measurements from device """
 		from random import random
 		update_size = round(random()*20)
 
-		self.current.extend(
-			self.measurementsFromList(np.random.random((update_size, 2))*10))
+		self._current.extend(
+			self._measurementsFromArray(np.random.random((update_size))*10))
 
-		self.voltage.extend(
-			self.measurementsFromList(np.random.random((update_size, 2))*50))
+		self._voltage.extend(
+			self._measurementsFromArray(np.random.random((update_size))*50))
 
-		self.power.extend(
-			self.measurementsFromList(np.random.random((update_size, 2))*500))
+		self._power.extend(
+			self._measurementsFromArray(np.random.random((update_size))*500))
+
+		self._time.extend(
+			self._measurementsFromArray(np.random.random((update_size))))
 
 	def getNewData(self):
-		self.updateMeasurements()
+		""" get updated measurements from device and return all recent """
+		self._updateMeasurements()
 
 		# update totals
-		self.current_history.extend(self.current)
-		self.voltage_history.extend(self.voltage)
-		self.power_history.extend(self.power)
+		self._current_history.extend(self._current)
+		self._voltage_history.extend(self._voltage)
+		self._power_history.extend(self._power)
+		self._time_history.extend(self._time)
 
 		# return data in dictionary
 		results = dict()
 
 		# use copy to stop dictionary contents being deleted also
-		results.update({'current' : self.current.copy()})
-		self.current.clear()
+		results.update({'current' : np.array(self._current.copy())})
+		self._current.clear()
 
-		results.update({'voltage' : self.voltage.copy()})
-		self.voltage.clear()
+		results.update({'voltage' : np.array(self._voltage.copy())})
+		self._voltage.clear()
 
-		results.update({'power' : self.power.copy()})
-		self.power.clear()
+		results.update({'power' : np.array(self._power.copy())})
+		self._power.clear()
+
+		results.update({'time' : np.array(self._time.copy())})
+		self._time.clear()
 
 		return results
 
 	def getAllDataNoUpdate(self):
+		""" return all data without updating from device """
 		# return data in dictionary
 		results = dict()
 
 		# use copy to stop dictionary contents being deleted also
-		results.update({'current' : self.current_history})
-		results.update({'voltage' : self.voltage_history})
-		results.update({'power' : self.power_history})
+		results.update({'current' : np.array(self._current_history)})
+		results.update({'voltage' : np.array(self._voltage_history)})
+		results.update({'power' : np.array(self._power_history)})
+		results.update({'time' : np.array(self._time_history)})
 		
 		return results
 
@@ -96,3 +92,7 @@ class DataStream:
 		self.getNewData()
 
 		return self.getAllDataNoUpdate()
+
+	def initializeCommunication(self, device=0):
+		""" begin serial comms with arduino or otherwise """
+		print("Initialized")
