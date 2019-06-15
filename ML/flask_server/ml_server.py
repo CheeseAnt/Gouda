00b15d3017c4f362df2249b41aa7f3ce99f93477
Anton <anton.shmatov@ucdconnect.ie>
2019-06-15 19:25:13 +0100

    Integrated mobilenet with flask server

--- ML/flask_server/ml_server.py
+++ ML/flask_server/ml_server.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from base64 import b64decode
 from hashlib import md5
+from mobilenet import MobileNet
+from os import remove as remove_file
 
 app =  Flask(__name__)
+mb = MobileNet()
+TEMP_FILENAME = "infer_input.jpg"
 
 @app.route("/")
 def main():
@@ -11,13 +15,17 @@ def main():
 @app.route("/infer", methods=["POST"])
 def infer_image():
     for value in request.values:
-        f = open("tempfile.jpg", "wb")
+        f = open(TEMP_FILENAME, "wb")
 
         base64encoded = value.replace('@', '=').replace('*', '+')
         base64decoded = b64decode(base64encoded)
 
         f.write(base64decoded)
         f.close()
+
+    mb.infer(TEMP_FILENAME)
+
+    remove_file(TEMP_FILENAME)
 
     return redirect("/")
 
